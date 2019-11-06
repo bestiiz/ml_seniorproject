6366f197390d7008cef1c5fb9d9e7494996c3d76
bestiiz <piyapong.bestiiz@mail.kmutt.ac.th>
2019-11-06 15:09:01 +0700

    Update mlproject.py

--- mlproject.py
+++ mlproject.py
@@ -136,5 +136,5 @@ def showResult():
     return render_template('result.html', filename=request.args.get('file'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
