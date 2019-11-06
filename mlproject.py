fdd15ab11b241a98d373e69eefe3ae0df1734289
bestiiz <piyapong.bestiiz@mail.kmutt.ac.th>
2019-11-06 12:43:00 +0700

    Update mlproject.py

--- mlproject.py
+++ mlproject.py
@@ -29,14 +29,14 @@ def upload_file1():
         # check if the post request has the file part
         if 'img1' not in request.files:
             flash('No file part')
-            return redirect(request.url_root)
+            return redirect(url_for('showData'))
         
         file = request.files['img1'] 
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url_root)
+            return redirect(url_for('showData'))
         if file and allowed_file(file.filename):
             filename = secure_filename(str(datetime.now()) + ".jpeg")
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
